f3d2b6e34d6b37b6be78a59510ac1e0af8b59d44
Yonatha <yonathalmeida@gmail.com>
2024-02-13 07:42:14 -0300

    rmeoção do keycloak

--- backend/src/app/app.module.ts
+++ backend/src/app/app.module.ts
@@ -3,25 +3,15 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SetorModule } from '../setores/setor.module';
-import { SetorEntity } from '../setores/setor.entity';
 
 import { ClienteModule } from '../clientes/cliente.module';
-import { ClienteEntity } from '../clientes/cliente.entity';
-import { ProjetoEntity } from 'src/projetos/projeto.entity';
 import { ProjetoModule } from 'src/projetos/projeto.module';
 import { UsuarioModule } from 'src/usuarios/usuario.module';
-import { AuthGuard, KeycloakConnectModule, ResourceGuard } from 'src/keycloak/src';
+import { AuthGuard, ResourceGuard } from 'src/keycloak/src';
 import { APP_GUARD } from '@nestjs/core';
-import * as session from 'express-session';
 
 @Module({
   imports: [
-    KeycloakConnectModule.register({
-      authServerUrl: 'http://localhost:8080/',
-      realm: 'agilmax',
-      clientId: 'erp-web',
-      secret: '34bj1W9wNVBYQsP9bkaUxV6JwwVBosxb',
-    }),
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: 'localhost',
--- backend/src/clientes/cliente.controller.ts
+++ backend/src/clientes/cliente.controller.ts
@@ -1,16 +1,14 @@
-import { Controller, Get, Param, ParseUUIDPipe, Post, Body, Put, Delete, Res, NotFoundException } from '@nestjs/common';
+import { Controller, Get, Param, ParseUUIDPipe, Post, Body, Put, Delete, NotFoundException } from '@nestjs/common';
 import { ClienteService } from './cliente.service';
 import { ClienteEntity } from './cliente.entity';
 import { ClienteResponseDto } from './cliente.response.dto';
 import { ClienteRequestDto } from './cliente.request.dto';
-import { Roles } from 'nest-keycloak-connect';
 
 @Controller('clientes')
 export class ClienteController {
   constructor(private readonly clienteService: ClienteService) { }
 
   @Get()
-  @Roles({ roles: ["CLIENTE_LISTAR"] })
   async findAll(): Promise<ClienteResponseDto[]> {
     const clientes = await this.clienteService.findAll();
     const clientesDto: ClienteResponseDto[] = clientes.map(cliente => cliente.toDto());
@@ -18,7 +16,6 @@ export class ClienteController {
   }
 
   @Get(':uuid')
-  @Roles({ roles: ["CLIENTE_EDITAR"] })
   async findOne(@Param('uuid') uuid: string): Promise<ClienteResponseDto> {
     const cliente = await this.clienteService.findOneByUuid(uuid);
     if (!cliente)
@@ -37,21 +34,18 @@ export class ClienteController {
   }
 
   @Post()
-  @Roles({ roles: ["CLIENTE_CADASTRAR"] })
   async create(@Body() request: ClienteRequestDto): Promise<ClienteResponseDto> {
     const createdCliente = await this.clienteService.create(request);
     return createdCliente.toDto();
   }
 
   @Put(':uuid')
-  @Roles({ roles: ["CLIENTE_EDITAR"] })
   async update(@Param('uuid', new ParseUUIDPipe({ version: '4' })) uuid: string, @Body() clienteEntity: ClienteEntity): Promise<string> {
     const updatedCliente = await this.clienteService.update(uuid, clienteEntity);
     return JSON.stringify(updatedCliente);
   }
 
   @Delete(':uuid')
-  @Roles({ roles: ["CLIENTE_EXCLUIR"] })
   async remove(@Param('uuid', new ParseUUIDPipe({ version: '4' })) uuid: string): Promise<ClienteResponseDto> {
     const cliente = await this.clienteService.remove(uuid);
     return cliente.toDto();
