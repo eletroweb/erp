e063b89f87ecf55bb4d9842cf72bcc56aa85d10f
Fábio <fabiomirandamonte@gmail.com>
2024-03-05 01:17:43 -0300

    Data inicio funcionando

--- frontend/cypress/e2e/contratos/ContratoCadastrar.spec.js
+++ frontend/cypress/e2e/contratos/ContratoCadastrar.spec.js
@@ -6,7 +6,14 @@ describe('Contratos', () => {
         cy.get('input[name="nome"').type('Serviço de Fundação')
         cy.get('input[placeholder="Orçamento"').type('20000')
         cy.get('input[placeholder="Data Início"]').click()
-        cy.get('.el-date-picker__body').should('be.visible')
+        //cy.get('.el-date-picker__body').should('be.visible') -- usado para fazer uma afirmação sobre um elemento da página ou sobre uma ação realizada no elemento.
         cy.get('.el-date-table-cell__text').contains('4').click()
+
+        cy.get('input[placeholder="Data Fim"]').click()
+        //cy.get('.el-date-picker__body').should('be.visible')
+        cy.get('.el-date-table-cell__text').contains('25').click()
+        
+
+
     })
 })
