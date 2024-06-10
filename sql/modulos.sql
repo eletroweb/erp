50ef7552575e2d0c290ebfdcf91aa3dcde240b3f
Yonatha <yonathalmeida@gmail.com>
2024-06-10 19:55:52 -0300

    Update colleciton e modulos.sql e roles.sql

---
+++ sql/modulos.sql
@@ -0,0 +1,5 @@
+INSERT INTO erp.modulos (uuid,nome,situacao) VALUES
+	 ('27526d6e-4dd4-4c41-af04-515321a4737b','Financeiro','ATIVO'),
+	 ('2f958501-6850-46e9-91ae-62176abd7b8c','Recursos Humanos','ATIVO'),
+	 ('f7c1af80-05af-4839-9b81-bd8a9c38faaa','Clientes','ATIVO'),
+	 ('92217e8c-fb1c-413f-8cb4-b26ceade72d3','Usuários','ATIVO');
