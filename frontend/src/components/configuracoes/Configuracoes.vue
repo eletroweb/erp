7bcf5583fbc0edb57788a9d0291ad63d06f43e70
Angela Talyta <87730446+tatazinhaang@users.noreply.github.com>
2024-08-03 12:49:05 -0300

    RF16.3.1 Habilitar/desabilitar notificações de despesas com vencimento nos proximos 5 dias

--- frontend/src/components/configuracoes/Configuracoes.vue
+++ frontend/src/components/configuracoes/Configuracoes.vue
@@ -6,17 +6,30 @@
         <el-tab-pane label="Usuários" v-if="usuarioLogadoStore.settings.has_company">
             <ListarUsuarios />
         </el-tab-pane>
+        <el-tab-pane label="Notificações">
+            <div class="notifications-tab">
+                <ToggleSwitch v-model="notificacoesDespesas" />
+                <label for="notificacoesDespesas">Notificações de despesas com vencimento</label>
+                <InputNumber v-if="notificacoesDespesas"v-model="notificacaoDias" :min="1" :max="30" />
+                <span v-if="notificacoesDespesas">dias</span>
+            </div>
+            <Button @click="salvarConfiguracoes" label="Salvar" />
+            <p v-if="errorMessage" class="error-message">{{ errorMessage }}</p>
+        </el-tab-pane>
     </el-tabs>
 </template>
 
 <script>
 import DataTable from 'primevue/datatable';
+import ToggleSwitch from 'primevue/toggleswitch';
+import InputNumber from 'primevue/inputnumber';
 import Column from 'primevue/column';
-import Tag from 'primevue/Tag';
-import Button from 'primevue/Button';
+import Tag from 'primevue/tag';
+import Button from 'primevue/button';
 import ListarUsuarios from '@/components/usuarios/ListarUsuarios.vue';
 import ConfiguracaoEmpresa from '@/components/configuracoes/ConfiguracaoEmpresa.vue';
 import { UsuarioLogadoStore } from '@/store/UsuarioLogadoStore'
+import { errorMessages } from 'vue/compiler-sfc';
 
 export default {
     components: {
@@ -25,7 +38,9 @@ export default {
         Tag,
         Button,
         ConfiguracaoEmpresa,
-        ListarUsuarios
+        ListarUsuarios,
+        ToggleSwitch,
+        InputNumber
     },
     setup() {
         const usuarioLogadoStore = UsuarioLogadoStore()
@@ -34,9 +49,49 @@ export default {
     },
     data() {
         return {
+            notificacoesDespesas: false,
+            notificacaoDias: 5,
+            errorMessage: '',
+            successMessage: ''
         };
     },
     mounted() {
+    },
+    methods: {
+        salvarConfiguracoes() {
+            this.errorMessage = '';
+            this.successMessage = '';
+
+            if(this.notificacoesDespesas) {
+                if(!this.notificacaoDias || this.notificacaoDias <= 0) {
+                    this.errorMessage = 'Preencha o campo dias';
+                    console.log("Erro: Preencha o campo dias");
+                    return;
+                }
+
+                this.successMessage = "Configurações salvas com sucesso."
+
+                console.log("Configurações salvas com sucesso")
+            } else {
+                console.log("Notificações desabilitadas");
+            }
+
+            
+        }
     }
 }
 </script>
+
+<style scoped>
+.notifications-tab {
+  display: flex;
+  align-items: center; 
+  gap: 10px; 
+  padding: 40px;
+  height: 200px;
+}
+
+.error-message {
+  color: red;
+}
+</style>
