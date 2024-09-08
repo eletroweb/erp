22dc6bb9435dd8c3f7840caa90c6d827495246d6
leandrodev01 <leandrosantos8877@gmail.com>
2024-09-07 22:17:25 -0300

    RF15.1 Filtrar despesas e receitas

--- frontend/src/components/financeiro/FinanceiroBarraDePesquisa.vue
+++ frontend/src/components/financeiro/FinanceiroBarraDePesquisa.vue
@@ -1,27 +1,25 @@
 <template>
-    <el-form-item class="search">
-        <div>
-            <h4>Buscar</h4>
-        </div>
+    <el-form-item  class="search">
+      
         <el-form-item>
             <el-input 
             type="text" name="pesquisa_descricao"
             v-model="financeiroStore.pesquisa.descricao"
             placeholder="Descrição..."
             @keyup.enter="financeiroStore.listar"
-            style="width: 335px; padding: 0% 10px" />
+            style="width: 205px; padding: 0% 4px " />
         </el-form-item>
 
         <el-form-item>
             <el-DatePicker 
-            format="DD/MM/YYYY" value-format="YYYY/MM/DD" inputId="data_vencimento" v-model="financeiroStore.pesquisa.data_fim" type="date" placeholder="Data vencimento" showIcon iconDisplay="input" @keyup.enter="financeiroStore.listar"
-            style="width: 335px; padding: 0% 10px" />
+            format="DD/MM/YYYY" value-format="YYYY/MM/DD" inputId="data_vencimento" v-model="financeiroStore.pesquisa.data_fim" type="date" placeholder="Data Início" showIcon iconDisplay="input" @keyup.enter="financeiroStore.listar"
+            style="width: 125px; padding: 0% 4px" />
         </el-form-item>
 
         <el-form-item>
             <el-DatePicker 
-            format="DD/MM/YYYY" value-format="YYYY/MM/DD" inputId="data_pagamento" v-model="financeiroStore.pesquisa.dataPagamentoInicio" type="date" placeholder="Data Pagamento" showIcon iconDisplay="input" @keyup.enter="financeiroStore.listar"
-            style="width: 335px; padding: 0% 10px" />
+            format="DD/MM/YYYY" value-format="YYYY/MM/DD" inputId="data_pagamento" v-model="financeiroStore.pesquisa.dataPagamentoInicio" type="date" placeholder="Data Fim" showIcon iconDisplay="input" @keyup.enter="financeiroStore.listar"
+            style="width: 125px; padding: 0% 4px" />
         </el-form-item>
 
         <el-form-item>
@@ -33,14 +31,7 @@
         </el-select>
         </el-form-item>
 
-        <el-form-item>
-            <el-select 
-            v-model="financeiroStore.pesquisa.parcelada" :options="parcelas" placeholder="Parcelas"
-            @keyup.enter="financeiroStore.listar">
-                <el-option v-for="parcela in parcelas" :key="parcela.value" :label="parcela.label" :value="parcela.value"/> 
-        </el-select>
-        </el-form-item>
-
+  
         <el-form-item>
             <el-select 
             v-model="financeiroStore.pesquisa.situacao" :options="situacoes" placeholder="Situação"
@@ -48,9 +39,12 @@
                 <el-option v-for="situacao in situacoes" :key="situacao.value" :label="situacao.label" :value="situacao.value"/> 
         </el-select>
         </el-form-item>
-
-        <el-button @click="financeiroStore.listar()">Pesquisar</el-button>
-        <el-button class="btn" @click="financeiroStore.limparPesquisa()">Limpar</el-button>
+        <div class="button-group ">
+            <Button class="btn-pesquisar" @click="financeiroStore.listar()" label="Pesquisar"/>
+            <Button  class="btn-limpar" @click="financeiroStore.limparPesquisa()" severity="secondary"  label="Limpar"/>
+            
+        </div>
+        
     </el-form-item>
 </template>
 
@@ -76,20 +70,7 @@ export default {
                     label: 'Despesa'
                 }
             ],
-            parcelas: [
-                { value: '1x', label: '1x' },
-                { value: '2x', label: '2x' },
-                { value: '3x', label: '3x' },
-                { value: '4x', label: '4x' },
-                { value: '5x', label: '5x' },
-                { value: 'SIM', label: '6x' },
-                { value: 'SIM', label: '7x' },
-                { value: 'SIM', label: '8x' },
-                { value: 'SIM', label: '9x' },
-                { value: 'SIM', label: '10x' },
-                { value: 'SIM', label: '11x' },
-                { value: 'SIM', label: '12x' }
-            ],
+    
             situacoes: [
                 { value: 'PAGA', label: 'PAGA' },
                 { value: 'PENDENTE', label: 'PENDENTE' },
@@ -103,26 +84,52 @@ export default {
 </script>
 
 <style scoped>
+
 .search {
     display: flex;
+    flex-wrap: wrap; 
     justify-content: flex-end;
     margin-bottom: 25px;
+    margin-top: 25px;
+    max-width: 100%;
 }
 
 .el-input {
     width: 100%;
     align-items: center;
+    margin-top: 8px;
+    margin-bottom: 8px;
 }
 
 .el-select {
-    width: 335px;
-    padding: 0% 10px
+    width: 135px;
+    padding: 0% 4px;
+    margin-top: 8px;
+    margin-bottom: 8px;
+  
 }
-
-.btn {
-    color: rgb(0, 151, 25);
-    border-color: rgb(21, 168, 45);
-    background-color: rgb(243, 243, 243);
+.button-group {
+    display: flex;
+    flex-direction: row-reverse;
+    gap: 8px;
+    
+}
+.btn-limpar {
+    color: #8f9091 ;
+    border:1px solid #f8fafc;
+    background-color: #eef6f9;
+    font-size: 14px;;
     width: 100px;
+    
 }
+ 
+.btn-pesquisar {
+    color: #ffffff;
+    background: #10b981;
+    border: 1px solid #10b981;
+    font-size: 14px;
+    width: 100px;
+} 
+
+
 </style>
